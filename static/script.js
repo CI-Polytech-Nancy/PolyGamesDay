a6a5078218b6cab0daee19181aa9bcc1b3c7cc7d
OtarieSupreme <gaby.mouchette@gmail.com>
2023-01-27 10:06:46 +0100

    PolyGamesDay 2023 - 1er commit

--- static/script.js
+++ static/script.js
@@ -28,3 +28,43 @@ window.onclick = function (event) {
         if (event.target === modal) closeModals();
     }
 }
+
+
+
+
+
+
+
+
+setInterval(function(){ plusSlides(1); }, 3000);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+showSlides(slideIndex = Math.floor(Math.random() * document.getElementsByClassName("mySlides").length) + 1); 
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+
+
+
+
+
