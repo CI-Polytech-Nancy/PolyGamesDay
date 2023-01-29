014b6c17170ac97dbd05a5a27d1bd6c24caa6dbd
OtarieSupreme <gaby.mouchette@gmail.com>
2023-01-29 09:29:59 +0100

    PGD 2023 Ajout P.S.

--- static/script.js
+++ static/script.js
@@ -59,12 +59,6 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "block";
 }
-
-
-
-
-
-
 
