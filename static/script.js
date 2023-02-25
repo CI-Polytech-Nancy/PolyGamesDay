5c9f04e47734078a3c6b5fcbc2fddbf710f5e999
MrSpaar <julienpistre.cagnes@gmail.com>
2023-02-25 12:16:42 +0100

    Cache based slideshow

--- static/script.js
+++ static/script.js
@@ -1,39 +1,27 @@
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topBtn").style.opacity = "1";
-    } else {
-        document.getElementById("topBtn").style.opacity = "0";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("topBtn").style.opacity = "1";
+  } else {
+      document.getElementById("topBtn").style.opacity = "0";
+  }
 };
 
 
-setInterval(function(){ plusSlides(1); }, 5000);
+setInterval(function(){ incrementSlideShow(1); }, 5000);
 
-var slideIndex = Math.floor(Math.random() * document.getElementsByClassName("mySlides").length) + 1;
-showSlides(slideIndex);
+function incrementSlideShow(inc) {
+  var img = document.getElementById("slide-img")
+  var idx = parseInt(img.alt)
 
+  nextIdx = (idx + inc) % 15
+  if (nextIdx == 0)
+    nextIdx = 15
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  img.src = "static/images/slideshow/" + nextIdx + ".webp"
+  img.alt = nextIdx
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-    slides[slideIndex-1].style.display = "block";
-}
-
